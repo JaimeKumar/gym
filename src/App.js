db604c54687fabe7d5c1fc8283b8349a4daefb5d
Jaime Kumar <jaimelacree@gmail.com>
2023-10-01 15:48:24 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import axios from 'axios'
+import { useEffect } from 'react';
 
-function App() {
+// const server = 'http://localhost:4000/'
+const server = 'https://gym-tracker-server-8ab6.onrender.com/'
+
+export default function App() {
+
+  const [data, setData] = useState(null)
+  const [newReps, setReps] = useState(0)
+
+  function addRep() {
+    axios.post(`${server}addRep`)
+      .then(res => {
+        console.log(res)
+        setReps(res.data.newReps)
+      })
+  }
+
+  function submitNew() {
+    let data = {
+      weight: document.getElementById('weight').value,
+      reps: document.getElementById('reps').value
+    }
+    axios.post(`${server}newWeight`, data)
+      .then((res) => {
+        console.log(res)
+        setData(res.data)
+      })
+  }
+
+  function getInfo() {
+    axios.get(`${server}getInfo`)
+      .then((res) => {
+        setData(res.data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  useEffect(() => {
+    getInfo();
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data? (
+        <div className='cont'>
+          <div className="block">
+            <h3>Muscle Group</h3>
+            <span>{data.part}</span>
+            <h3>Previous Weight</h3>
+            <span>{data.weight}</span>
+            <h3>Reps</h3>
+            <span>{data.reps}</span>
+          </div>
+          
+          <div className="block">
+            <h3>New Weight</h3>
+            <input id='weight' type="number" defaultValue={data.weight} />
+            <h3>Reps Today</h3>
+            <div className="row">
+              <input id='reps' className='newReps' type="number" value={newReps} />
+              <button onClick={addRep}>+</button>
+            </div>
+            <br />
+            <br />
+            <button className='submit' onClick={submitNew}>UPDATE</button>
+          </div>
+        </div>
+      ) :''}
     </div>
   );
 }
-
-export default App;
